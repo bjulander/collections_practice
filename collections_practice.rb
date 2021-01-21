dc6a3a0bb51c94fb07bba92a982a43d78200a0b5
Blake Julander <13lakejulander@gmail.com>
2021-01-21 12:06:06 -0700

    commit tree 15331271c002a0c9618b5b08f5dd9b4b859f377d

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,43 @@
+def sort_array_asc(array)
+    array.sort
+ end
 
+ def sort_array_desc(array)
+   array.sort.reverse
+ end
+
+ def sort_array_char_count(array)
+   array.sort {|a,b| a.length <=> b.length}
+ end
+
+ def swap_elements(array)
+   array[1],array[2]=array[2],array[1]
+   return array
+ end
+
+ def reverse_array(array)
+   array.reverse.each {|v| v}
+ end
+
+def kesha_maker(array)
+    array.each {|element| element[2] = "$"}     
+end
+
+def find_a(array)
+    array.select{|element| element[0] =="a"}
+end
+
+def sum_array(array)
+    array.inject {|add,num| add+num}
+ end
+
+ def add_s(array)
+  array.each_with_index.collect do |word, index|
+    if index != 1 
+      word = word + "s"
+    else
+      word
+    end
+  end
+end
+  
